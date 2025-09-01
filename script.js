0ff1187c1fe2f6ba136139378567b0b27e9eea71
Daniel <ti.daniel76@gmail.com>
2025-09-01 15:45:05 -0300

    quarto commit

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 function consultaCEP(event) {
     if (event) event.preventDefault();
 
-    let cep = document.getElementById('cep').value 
-    let url = 'https://viacep.com.br/ws/' + cep + '/json/'
+    let cep = document.getElementById('cep').value;
+    let url = 'https://viacep.com.br/ws/' + cep + '/json/';
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('logradouro').value = data.logradouro
-            document.getElementById('bairro').value = data.bairro
-            document.getElementById('localidade').value = data.localidade
-            document.getElementById('uf').value = data.uf
+            document.getElementById('logradouro').value = data.logradouro;
+            document.getElementById('bairro').value = data.bairro;
+            document.getElementById('localidade').value = data.localidade;
+            document.getElementById('uf').value = data.uf;
+
+            localStorage.setItem('endereco', JSON.stringify(data));
         })
-        .catch(error => console.error(error))
+        .catch(error => console.error(error));
 }
 
 window.onload = () => {
